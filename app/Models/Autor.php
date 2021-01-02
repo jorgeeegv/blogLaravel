e410d6a13d3fa2a30ec25c7b5e26e6af72e0c6a9
Jorge <jorgegvilla98@gmail.com>
2021-01-02 13:32:44 +0100

    Autor controller y model

---
+++ app/Models/Autor.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Autor extends Model
+{
+    use HasFactory;
+
+    protected $table = 'autors';
+
+    public function posts(){
+        return $this->hasMany('App\Models\Post');
+    }
+
+
+
+}
