69514325a4f32a66fcdb9ad043bc7c212058bb45
Jorge <jorgegvilla98@gmail.com>
2021-01-07 11:31:28 +0100

    Migraciones de autores y posts

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,9 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
-    <a href=" {{ url('/') }} " class="btn btn-dark">Inicio</a>
+    <div class="ml-5">
+    <a href=" {{ url('/') }} " class="btn btn-secondary">Inicio</a>
     &nbsp;&nbsp;
-    <a href="{{ route('post.index') }}" class="btn btn-dark">Lista de POSTS</a>
+    <a href="{{ route('post.index') }}" class="btn btn-secondary">Listado de Posts</a>
     &nbsp;&nbsp;
-    <a href="{{ route('post.create') }}" class="btn btn-dark">Crear POST</a>
+    <a href="{{ route('post.create') }}" class="btn btn-secondary">Crear Post</a>
     &nbsp;&nbsp;
+    </div>
     <b class="ml-auto text-white">{{ fechaActual()}}</b>
 </nav>
